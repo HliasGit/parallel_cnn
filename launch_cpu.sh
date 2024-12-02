b6c6ecb0526741aaa92227cb60fc25d6af2cd579
LukeTheWalker <lucag2001@gmail.com>
2024-12-02 11:57:56 +0100

    FEAT: pushed main notebook

---
+++ launch_cpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=ensemble_forecasters
+#SBATCH --output={logs_folder}/output_%j.txt
+#SBATCH --error={logs_folder}/error_%j.txt
+#SBATCH --nodes={num_nodes}
+#SBATCH --ntasks-per-node={num_tasks_per_node}
+#SBATCH --ntasks-per-socket=16
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:30:00
+
+cd {current_dir}
+eval "$(micromamba shell hook --shell bash)"
+micromamba activate ensemble_venv
+module load mpi/OpenMPI/4.0.5-GCC-10.2.0
+mpirun -n {world_size} python parallel_cnn.py {data_folder}
